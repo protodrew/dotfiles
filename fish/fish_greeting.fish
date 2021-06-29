6c3c47aa85ca59d7baf39fa71cb6b56e99003346
Protodrew <contact.protodrew@pm.me>
2021-06-29 17:50:00 -0500

    change betterdiscord theme

--- fish/fish_greeting.fish
+++ fish/fish_greeting.fish
@@ -3,4 +3,5 @@ neofetch
 alias ls 'natls'
 alias up 'sudo apt update && sudo apt upgrade && flatpak update && sudo apt autoremove && sudo apt autoclean'
 alias clear 'clear && neofetch'
+alias spicetify '/home/protodrew/spicetify-cli/spicetify'
 end
